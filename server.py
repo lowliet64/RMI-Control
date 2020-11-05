27ede3f9844cdee331fce786fefc5f51ef663642
gaboHolanda <60978159+gaboHolanda@users.noreply.github.com>
2020-11-05 20:56:26 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,26 @@
 import Pyro4
+import time
+import aviao
 
 @Pyro4.expose
+
 class Aviao:
-    def saludar(self):
-        return 'aviao sei la'
+    def __init__(self, nome):
+        self.posicaoAtual = 0
+        self.nome = nome
+
+    def irAteDestino(self, posicaoDestino):
+        distancia = abs(self.posicaoAtual - posicaoDestino)
+        while True:
+            if self.posicaoAtual < posicaoDestino:
+                self.posicaoAtual++
+                print(self.posicaoAtual)
+            elif self.posicaoAtual > posicaoDestino:
+                self.posicaoAtual--
+                print(self.posicaoAtual)
+            elif self.posicaoAtual = posicaoDestino:
+                return "O avião chegou ao seu destino"
+            time.sleep(1)
 
 
 daemon = Pyro4.Daemon()
